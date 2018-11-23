b606c7d81513ce08d89d6a684888547e9a95fe82
gjgd <gjgd@protonmail.com>
2018-11-23 09:37:22 +0100

    Add utils file for tests

---
+++ test/utils.js
@@ -0,0 +1,31 @@
+module.exports.assertFail = async (promise, message) => {
+  try {
+    await promise;
+    assert(false);
+  } catch (e) {
+    if (e.name == 'AssertionError') {
+      if (message) {
+        assert(false, message);
+      } else {
+        assert(false);
+      }
+    }
+  }
+};
+
+class BlockMiner {
+  async mine(numberOfBlocks) {
+    for (let i = 0; i < numberOfBlocks; i++) {
+      await new Promise((resolve, _) => {
+        web3.currentProvider.sendAsync({method: 'evm_mine', id: i}, resolve);
+      });
+    }
+  }
+
+  async mineUntilBlock(blockNumber) {
+    const padding = blockNumber - web3.eth.blockNumber;
+    await this.mine(padding);
+  }
+}
+
+module.exports.blockMiner = new BlockMiner();
