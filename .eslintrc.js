bc6c1ca3ea48e1a508f007b435e31aba4589ad90
gjgd <gjgd@protonmail.com>
2018-11-24 10:22:05 +0100

    Add eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "eslint-config-google",
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true
+        }
+    },
+    "rules": {
+        "max-len": "off",
+        "require-jsdoc": "off",
+    }
+};
