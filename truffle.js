72df7f628a9349715b109653ed06e22d2dd8a9d8
gjgd <gjgd@protonmail.com>
2018-12-21 11:54:31 +0100

    Fix solc version

--- truffle.js
+++ truffle.js
@@ -1,9 +1,14 @@
 module.exports = {
+  compilers: {
+    solc: {
+      version: '^0.4.24',
+    },
+  },
   networks: {
     local: {
       host: 'localhost',
       port: 8545,
       network_id: '*',
-    }
-  }
+    },
+  },
 };
